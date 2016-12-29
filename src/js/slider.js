de56143598037f4cd93e21fdad53cd0f9fecfd18
YuYingDe <yyd-yyd@hotmail.com>
2016-12-29 23:10:46 +0800

    添加幻灯片代码

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,8 +5,42 @@ export default {
         }
     },
     mounted() {
-        this.$nextTick(function () {
-            
-        })
+        jssor_1_slider_init();
     }
 }
+
+let jssor_1_slider_init = function () {
+    let jssor_1_options = {
+        $AutoPlay: false,
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        },
+        $ThumbnailNavigatorOptions: {
+            $Class: $JssorThumbnailNavigator$,
+            $Cols: 9,
+            $SpacingX: 3,
+            $SpacingY: 3,
+            $Align: 260
+        }
+    };
+
+    let jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*responsive code begin*/
+    /*you can remove responsive code if you don't want the slider scales while window resizing*/
+    function ScaleSlider() {
+        let refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 900);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $Jssor$.$AddEvent(window, "load", ScaleSlider);
+    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+    /*responsive code end*/
+};
